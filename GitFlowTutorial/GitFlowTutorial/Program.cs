c728662a8a0cd84dc1cb550dfca91a8f0cb446b9
shankur mahajan <shankurm@slalom.com>
2016-02-01 16:23:42 -0600

    Add the console app to repo

---
+++ GitFlowTutorial/GitFlowTutorial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitFlowTutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
